84969c25eab0c4f4c926a826d19cdebedc11ee9d
kfrapin <kevin.frapin@gmail.com>
2021-10-06 17:08:43 +0200

    update example: create resource

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,7 @@
-data "zeflix_catalog" "my_catalog" {
-    # required parameter:
-    # id = "<catalog-id>"
+resource "zeflix_catalog" "top_100_catalog" {
+    name = "Top 100 Best Movies Ever"
 }
 
 output "my_catalog" {
-    value = data.zeflix_catalog.my_catalog
+    value = data.zeflix_catalog.top_100_catalog
 }
